cc55ec0e872f43960b46c95534760c8616788878
kwangc <lkhsoft@gmail.com>
2019-01-03 11:27:18 +0900

    bmiCalculator using Express

--- 7. Node.js/my-express-server/server.js
+++ 7. Node.js/my-express-server/server.js
@@ -4,7 +4,19 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-  res.send("<h1>Hello</h1>");
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/contact", function(req, res) {
+  res.send("Contact me at: tony@gmail.com");
+});
+
+app.get("/about", function(req, res) {
+  res.send("My name is Tony and I love technologies");
+});
+
+app.get("/hobbies", function(req, res) {
+  res.send("<ul><li>Coffee</li><li>Code</li><li>Rubik's Cube</li></ul>");
 });
 
 app.listen(3000, function() {
