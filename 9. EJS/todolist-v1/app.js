91eb2e1c6467506875f505c751dff3dcd8ee6651
kwangc <lkhsoft@gmail.com>
2019-01-07 18:10:17 +0900

    Adding work list

--- 9. EJS/todolist-v1/app.js
+++ 9. EJS/todolist-v1/app.js
@@ -6,8 +6,10 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res) {
@@ -22,15 +24,29 @@ app.get("/", function(req, res) {
   let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items
   });
 });
 
 app.post("/", function(req, res) {
+
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+
+  if(req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
 });
 
 app.listen(3000, function() {
