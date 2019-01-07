37977a10df83d7d60fa40aa1f7a57e09d9616939
kwangc <lkhsoft@gmail.com>
2019-01-07 18:22:56 +0900

    Adding header and footer

--- 9. EJS/todolist-v1/app.js
+++ 9. EJS/todolist-v1/app.js
@@ -49,6 +49,10 @@ app.get("/work", function(req, res) {
   });
 });
 
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
 });
