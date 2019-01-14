6be25deb03552d8e87f95d916bd8a9a61f4b3031
kwangc <lkhsoft@gmail.com>
2019-01-14 17:43:54 +0900

    Until Challenge 15

--- 9. EJS/Challenge 3 - Blog/app.js
+++ 9. EJS/Challenge 3 - Blog/app.js
@@ -15,9 +15,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+var posts = [];
+
 app.get('/', function(req, res) {
   res.render('home', {
-    startingContent: homeStartingContent
+    startingContent: homeStartingContent,
+    posts: posts
   });
 });
 
@@ -33,7 +36,18 @@ app.get('/contact', function(req, res) {
   });
 });
 
+app.get('/compose', function(req, res){
+  res.render('compose');
+});
 
+app.post('/compose', function(req, res) {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect('/');
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
