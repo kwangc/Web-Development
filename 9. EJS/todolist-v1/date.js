5a33dbcf1ace8028846ab74cc70396f9a835c17e
Tony Lee <tony.lee@lineable.net>
2020-07-29 13:55:41 +0900

    todolistv1 app

--- 9. EJS/todolist-v1/date.js
+++ 9. EJS/todolist-v1/date.js
@@ -1,12 +1,11 @@
-//jshint esversion:6
-
 exports.getDate = function() {
   const today = new Date();
+
   const options = {
     weekday: "long",
     day: "numeric",
     month: "long"
-  };
+  }
 
   return today.toLocaleDateString("en-US", options);
 };
@@ -15,8 +14,8 @@ exports.getDay = function() {
   const today = new Date();
 
   const options = {
-    weekday: "long"
-  };
+    weekday: "long",
+  }
 
   return today.toLocaleDateString("en-US", options);
 };
